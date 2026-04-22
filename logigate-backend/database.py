e9acca7b2b504df7c75bff5efbd15ec7d9be992f
Joshua <joshua@example.com>
2026-04-22 12:44:06 -0600

    venegas eres tan gaycito y hermosito

--- logigate-backend/database.py
+++ logigate-backend/database.py
@@ -2,9 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Estructura: postgresql://usuario:contraseña@host:puerto/nombre_bd
-# Cambia la IP por 127.0.0.1 (localhost)
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:S1stemas24@localhost:5432/logigate"
+# URL de conexion con la IP detectada en pgAdmin
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:S1stemas24@192.168.100.47:5432/logigate"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
@@ -12,7 +11,7 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 def init_db():
-    from models import Registro, User  # noqa: F401 — importar para registrar los modelos
+    from models import Registro, User
     Base.metadata.create_all(bind=engine)
 
 def get_db():
